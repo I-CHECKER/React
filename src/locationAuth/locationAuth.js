592711ad1d8694b0d26ec5ac5c860cd679f694c0
Jo Eunsang <sang11032@gmail.com>
2023-09-01 21:20:45 +0900

    현재 위치를 받아 오는 locationAuth, 카카오맵을 랜더링하는 mapRender 추가

---
+++ src/locationAuth/locationAuth.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import { useGeolocated } from "react-geolocated";
+import { Wrapper, GoogleMap, Marker } from "@googlemaps/react-wrapper";
+import MapRender from "./mapRender";
+
+const LocationAuth = () => {
+    const {
+        coords: initialCoords,
+        isGeolocationAvailable,
+        isGeolocationEnabled,
+        positionError,
+        timestamp,
+    } = useGeolocated({
+        positionOptions: {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: Infinity,
+        },
+        watchPosition: false,
+        userDecisionTimeout: null,
+        suppressLocationOnMount: false,
+        geolocationProvider: navigator.geolocation,
+        isOptimisticGeolocationEnabled: true,
+        watchLocationPermissionChange: false,
+    });
+
+    // timestamp를 Date 객체로 변환
+    const timestampDate = timestamp ? new Date(timestamp) : null;
+    // 날짜 형식으로 변환 (예: "2023-09-01 14:30:00")
+    const timestampString = timestampDate ? timestampDate.toLocaleString() : "N/A";
+
+    // 기준 위도, 경도 및 오차 범위 설정
+    const baseLatitude = 37.3387;
+    const baseLongitude = 126.7345;
+    const errorRange = 0.000002;
+
+    const [currentCoords, setCurrentCoords] = useState(initialCoords);
+    const [isWithinRange, setIsWithinRange] = useState(false);
+
+    // 현재 위치와 기준 위치 간의 거리 계산
+    const calculateDistance = () => {
+        if (currentCoords) {
+            const distance = Math.sqrt(
+                Math.pow(currentCoords.latitude - baseLatitude,3) +
+                Math.pow(currentCoords.longitude - baseLongitude,3)
+            );
+            return distance;
+        }
+        return null;
+    };
+
+    const handleSyncLocation = () => {
+        setCurrentCoords(initialCoords);
+    };
+
+    useEffect(() => {
+        const distance = calculateDistance();
+        // 오차 범위 내에 위치한 경우
+        if (distance !== null && distance <= errorRange) {
+            setIsWithinRange(true);
+        } else {
+            setIsWithinRange(false);
+        }
+    }, [currentCoords]);
+
+    return !isGeolocationAvailable ? (
+        <div>Your browser does not support Geolocation</div>
+    ) : !isGeolocationEnabled ? (
+        <div>Geolocation is not enabled</div>
+    ) : (
+        <div>
+            <table>
+                <tbody>
+                <tr>
+                    <td>위도</td>
+                    <td>{currentCoords?.latitude || "N/A"}</td>
+                </tr>
+                <tr>
+                    <td>경도</td>
+                    <td>{currentCoords?.longitude || "N/A"}</td>
+                </tr>
+                <tr>
+                    <td>정확도</td>
+                    <td>{currentCoords?.accuracy || "N/A"}</td>
+                </tr>
+                <tr>
+                    <td>날짜</td>
+                    <td>{timestampString}</td>
+                </tr>
+                <tr>
+                    <td>
+                        <MapRender
+                            latitude={currentCoords?.latitude || baseLatitude}
+                            longitude={currentCoords?.longitude || baseLongitude}
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>연구실이 근처에 {isWithinRange ? "있습니다." : "없습니다."}</td>
+                </tr>
+                </tbody>
+            </table>
+            <button onClick={handleSyncLocation}>동기화</button>
+        </div>
+    );
+};
+
+export default LocationAuth;
---
+++ src/locationAuth/mapRender.js
@@ -0,0 +1,37 @@
+// MapRender.js
+import React, { useEffect } from "react";
+
+const { kakao } = window;
+
+const MapRender = ({ latitude, longitude }) => {
+    useEffect(() => {
+        const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+        const locPosition = new kakao.maps.LatLng(latitude, longitude); // 현재 위치의 좌표
+        const options = {
+            center: locPosition, // 지도의 중심 좌표를 현재 위치로 설정
+            level: 3, // 지도의 레벨(확대, 축소 정도)
+        };
+        const map = new kakao.maps.Map(container, options);
+
+        // 현재 위치를 나타내는 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: locPosition,
+        });
+
+        var iwRemoveable = true;
+
+    }, [latitude, longitude]);
+
+    return (
+        <div
+            id="map"
+            style={{
+                width: "500px",
+                height: "500px",
+            }}
+        ></div>
+    );
+};
+
+export default MapRender;
