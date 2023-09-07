a0a504f7785d6b4bfd49bce3907f344a247026de
bnh <skagus159632@naver.com>
2023-09-07 21:49:33 +0900

    xlsx download

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LocationAuth from "./locationAuth/locationAuth";
 import MapRender from "./locationAuth/mapRender";
+import CsvDownload from "./csvDownload/csvDownload";
+
 
 function App() {
   return (
     <div className="App">
-        <div>
-            <LocationAuth/>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LocationAuth/>}></Route>
+                <Route path="/csv" element={<CsvDownload/>}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
